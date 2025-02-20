c2b940ff5afe36537ff1751cf173f57e519bc19f
Manuel Gurbanov <manuelgurbanov@gmail.com>
2025-02-19 23:26:31 -0300

    test fix not found image

--- src/MainBanner.js
+++ src/MainBanner.js
@@ -5,10 +5,9 @@ export default function MainBanner() {
     const langua = language.language;
     return (
         <div className="w-screen flex flex-col items-center justify-center mt-12">
+            <img className="max-[450px]:hidden w-screen object-cover" src={"desktopBanner_" + langua + ".webp"} alt="bg" data-aos="fade-up"/>
 
-            <img className="max-[450px]:hidden w-screen object-cover" src={"desktopBanner" + langua + ".webp"} alt="bg" data-aos="fade-up"/>
-
-            <img className="hidden max-[450px]:block w-screen object-cover" src={"phoneBanner" + langua + ".webp"} alt="bg" data-aos="fade-up" />
+            <img className="hidden max-[450px]:block w-screen object-cover" src={"phoneBanner_" + langua + ".webp"} alt="bg" data-aos="fade-up" />
         </div>
     );
 }
